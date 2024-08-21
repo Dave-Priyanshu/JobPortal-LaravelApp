1a90da71e9be6464abb4ce032108a4e85308dbe8
Dave-Priyanshu <ims30priyanshu@gmail.com>
2024-08-21 18:45:03 +0530

    Retrieve Static Data from model

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 Route::get('/', function () {
+    return view('listings',[
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+//Single listing
+Route::get('/singleListing/{id}',function($id){
     return view('listing',[
-        'heading' => 'Latest Listing',
-        'listings' => [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
-            ]
-        ]
+        'listing' => Listing::find($id)
     ]);
 });
