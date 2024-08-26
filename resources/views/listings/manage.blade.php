444390798a41b30e2e6c2513cc3a38ecec1250e0
Dave-Priyanshu <ims30priyanshu@gmail.com>
2024-08-26 17:49:47 +0530

    Added Image in the manage post section & some Design Changes

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -1,62 +1,59 @@
 <x-layout>
-    <x-card class="p-10">
-        <header>
-            <h1
-                class="text-3xl text-center font-bold my-6 uppercase"
-            >
-                Manage Gigs
+    <x-card class="p-12 bg-white shadow-lg rounded-lg">
+        <header class="bg-gray-100 p-6 rounded-md">
+            <h1 class="text-4xl text-center font-bold my-4 uppercase text-blue-600">
+                Manage Posts
             </h1>
+            <p class="text-center text-gray-600">Manage your job listings effectively</p>
         </header>
 
-        <table class="w-full table-auto rounded-sm">
+        <table class="w-full table-auto bg-white rounded-md shadow-md mt-6">
+            <thead class="bg-blue-100 text-blue-600">
+                <tr>
+                    <th class="px-4 py-3 text-left">Image</th>
+                    <th class="px-4 py-3 text-left">Job Title</th>
+                    <th class="px-4 py-3 text-center">Edit</th>
+                    <th class="px-4 py-3 text-center">Delete</th>
+                </tr>
+            </thead>
             <tbody>
                 @unless($listings->isEmpty())
                 @foreach($listings as $listing)
-                    
-                
-                <tr class="border-gray-300">
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <a href="show.html">
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                    <td class="px-4 py-4 text-lg text-gray-800">
+                        <a href="show.html" class="font-semibold hover:text-blue-500">
+                            
+                            <img src="{{$listing->logo ? asset('storage/'. $listing->logo): asset('/images/no-image.png')}}" class="w-16 h-16 object-cover rounded-md">
+                        </a>
+                    </td>
+                    <td class="px-4 py-4 text-lg text-gray-800">
+                        <a href="show.html" class="font-semibold hover:text-blue-500">
                             {{$listing->title}}
                         </a>
                     </td>
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <a
-                            href="/listings/{{$listing->id}}/edit"
-                            class="text-blue-400 px-6 py-2 rounded-xl"
-                            ><i
-                                class="fa-solid fa-pen-to-square"
-                            ></i>
-                            Edit</a
-                        >
+                    <td class="px-4 py-4 text-center">
+                        <a href="/listings/{{$listing->id}}/edit" class="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md">
+                            <i class="fa-solid fa-pen-to-square"></i> Edit
+                        </a>
                     </td>
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                    <form method="POST" action="/listings/{{$listing->id}}">
-                        @csrf
-                        @method('DELETE')
-                        <button class="text-red-500"><i class="fa-solid fa-trash mr-1"></i>Delete</button>
-                        {{-- <buttton class="text-red-500"><i class="fa-solid fa-trash"></i>Delete</buttton> --}}
+                    <td class="px-4 py-4 text-center">
+                        <form method="POST" action="/listings/{{$listing->id}}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="text-white bg-red-500 hover:bg-red-600 px-4 py-2 rounded-md">
+                                <i class="fa-solid fa-trash mr-1"></i> Delete
+                            </button>
                         </form>
                     </td>
                 </tr>
                 @endforeach
-
                 @else
-                <tr class="border-gray-300">
-                    <td class="px-4 py-8 border-t border-gray-300 text-lg">
-                        <p class="text-center">No Listings Found</p>
+                <tr>
+                    <td class="px-4 py-8 text-lg text-center text-gray-500" colspan="4">
+                        <p>No Listings Found</p>
                     </td>
                 </tr>
-                    
                 @endunless
-
-               
             </tbody>
         </table>
     </x-card>
